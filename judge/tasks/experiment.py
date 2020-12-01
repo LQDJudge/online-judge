1004563a53d3ac9ee724c6e5b7154618b30a4468
cuom1999 <lephuocdinh99@gmail.com>
2020-12-01 17:18:17 -0600

    Temporary fix for abort race

---
+++ judge/tasks/experiment.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from judge.models import SubmissionTestCase, Problem
+
+
+def generate_report(problem):
+    testcases = SubmissionTestCase.objects.filter(submission__problem=problem).all()
+    
+    score = defaultdict(int)
+    total = defaultdict(int)
+    rate = defaultdict(int)
+
+    for case in testcases.iterator():
+        score[case.case] += int(case.status == 'AC')
+        total[case.case] += 1
+
+    for i in score:
+        rate[i] = score[i] / total[i]
+
+    for i, _ in sorted(rate.items(), key=lambda x: x[1], reverse=True):
+        print(i, score[i], total[i], rate[i])
+
+def test(code):
+    problem = Problem.objects.get(code=code)
+    generate_report(problem)
--- judge/views/submission.py
+++ judge/views/submission.py
@@ -233,8 +233,12 @@ class SubmissionSourceRaw(SubmissionSource):
 @require_POST
 def abort_submission(request, submission):
     submission = get_object_or_404(Submission, id=int(submission))
-    if (not request.user.is_authenticated or (submission.was_rejudged or (request.profile != submission.user)) and
-            not request.user.has_perm('abort_any_submission')):
+    # if (not request.user.is_authenticated or (submission.was_rejudged or (request.profile != submission.user)) and
+    #         not request.user.has_perm('abort_any_submission')):
+    #     raise PermissionDenied()
+    if (not request.user.is_authenticated 
+            or submission.was_rejudged
+            or not request.user.has_perm('abort_any_submission')):
         raise PermissionDenied()
     submission.abort()
     return HttpResponseRedirect(reverse('submission_status', args=(submission.id,)))
