0adbc74d951eacc076755e56ac6aed474d5cc9f9
cuom1999 <lephuocdinh99@gmail.com>
2020-12-29 20:29:50 -0600

    Add full name to user tables

--- judge/views/organization.py
+++ judge/views/organization.py
@@ -19,7 +19,7 @@ from reversion import revisions
 from judge.forms import EditOrganizationForm
 from judge.models import BlogPost, Comment, Organization, OrganizationRequest, Problem, Profile, Contest
 from judge.utils.ranker import ranker
-from judge.utils.views import TitleMixin, generic_message
+from judge.utils.views import TitleMixin, generic_message, QueryStringSortMixin, DiggPaginatorMixin
 
 __all__ = ['OrganizationList', 'OrganizationHome', 'OrganizationUsers', 'OrganizationMembershipChange',
            'JoinOrganization', 'LeaveOrganization', 'EditOrganization', 'RequestJoinOrganization',
@@ -117,18 +117,25 @@ class OrganizationHome(OrganizationDetailView):
         return context
 
 
-class OrganizationUsers(OrganizationDetailView):
+class OrganizationUsers(QueryStringSortMixin, OrganizationDetailView):
     template_name = 'organization/users.html'
-
+    all_sorts = frozenset(('points', 'problem_count', 'rating', 'performance_points'))
+    default_desc = all_sorts
+    default_sort = '-performance_points'
+        
     def get_context_data(self, **kwargs):
         context = super(OrganizationUsers, self).get_context_data(**kwargs)
         context['title'] = _('%s Members') % self.object.name
-        context['users'] = \
-            ranker(self.object.members.filter(is_unlisted=False).order_by('-performance_points', '-problem_count')
-                   .select_related('user').defer('about', 'user_script', 'notes'))
         context['partial'] = True
         context['is_admin'] = self.can_edit_organization()
         context['kick_url'] = reverse('organization_user_kick', args=[self.object.id, self.object.slug])
+
+        context['users'] = ranker(
+                self.get_object().members.filter(is_unlisted=False).order_by(self.order, 'id').select_related('user') \
+                    .only('display_rank', 'user__username', 'points', 'rating', 'performance_points', 'problem_count')
+            )
+        context['first_page_href'] = '.'
+        context.update(self.get_sort_context())
         return context
 
 
