551c9c35035159b1d550582d8b4c676dbe0c4809
Bui Tuan Dung <dungbuit1k28@gmail.com>
2022-12-19 08:01:02 +0700

    modify resolver link

--- judge/views/resolver.py
+++ judge/views/resolver.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
 from django.http import HttpResponseForbidden
 
-def resolver(request):
-    if request.user.is_superuser:
-        return render(
-            request,
-            "resolver/resolver.html",
-            {
-                "title": _("Resolver"),
-            },
-        )
-    return HttpResponseForbidden()
+class Resolver(TemplateView):
+    title = _("Resolver")
+    template_name = "resolver/resolver.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Resolver, self).get_context_data(**kwargs)
+        context["contest_json"] = "/static/contest.json"
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return super(Resolver, self).get(request, *args, **kwargs)
+        return HttpResponseForbidden()
+
     
